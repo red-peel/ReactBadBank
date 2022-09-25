a13bd70064f6c0622382fa0830fed286227f013e
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-09-24 20:54:35 -0500

    Create footer.js

---
+++ bad-bank-starter/footer.js
@@ -0,0 +1,10 @@
+function Footer(){
+    return (
+    <footer class="footer" >
+        <div class="container">
+          <a class="text-muted" href="https://github.com/red-peel">made by E.G.</a>
+        </div>
+      </footer>   
+    );  
+  }
+  
