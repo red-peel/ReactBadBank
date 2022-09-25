f0c85a187a3929a3204f2bbc8c24d027a26ff8ea
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-09-24 23:30:22 -0500

    Update footer.js

--- bad-bank-starter/footer.js
+++ bad-bank-starter/footer.js
@@ -1,8 +1,8 @@
 function Footer(){
     return (
-    <footer class="footer" >
-        <div class="container">
-          <a class="text-muted" href="https://github.com/red-peel">made by E.G.</a>
+    <footer className="footer" >
+        <div className="container">
+          <a className="text-muted" href="https://github.com/red-peel">made by E.G.</a>
         </div>
       </footer>   
     );  
