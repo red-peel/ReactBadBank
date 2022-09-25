46058a873229968c69de73ec617b2a08004eda27
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-09-25 14:13:52 -0500

    Initialize deposit functionality

--- bad-bank-starter/deposit.js
+++ bad-bank-starter/deposit.js
@@ -1,5 +1,34 @@
-function Deposit(){
+function Deposit() {
+  const [show, setShow] = React.useState(false);
+  const [status, setStatus] = React.useState('');
+  const [depositAmount, setEmail] = React.useState('');
+  const ctx = React.useContext(UserContext);
+
+
+  function handleDeposit() {
+    /* depositAmount is handled here. first verify that depositAmount is valid, 
+    then add depositAmount to ctx.users.balance of ctx.current.username */
+  }
+
+
+
+
   return (
-    <h1>Deposit</h1>
+    <Card
+      bgcolor="dark primary"
+      header="Deposit"
+      status={status}
+      body={(true)?(
+        <>
+          <h3>Account Total: 123456</h3>
+          <br />
+          <h4>Deposit Amount</h4>
+          <input type="input" className="form-control" id="deposit" placeholder="Enter Amount for Deposit" value={depositAmount} />
+          <br />
+          <button type="submit" className="btn btn-light" onClick={handleDeposit}>Confirm</button>
+        </>
+      ):(<h1>Please log in to deposit.</h1>)
+    }
+    />
   )
 }
