e1e9ca5a44f43a45c9589062679ea2b320e6623b
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-09-24 20:56:38 -0500

    Update navbar.js

--- bad-bank-starter/navbar.js
+++ bad-bank-starter/navbar.js
@@ -2,14 +2,14 @@ function NavBar(){
   return(
     <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="#"><img src="bank.png" className="inlineImage"></img> bad bank.</a>
+      <a className="navbar-brand" href="#"><img src="bank.png" className="inlineImage"></img> bad bank.™</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="#/CreateAccount/">Create Account</a>
+            <a className="nav-link" href="#/CreateAccount/" >Create Account</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#/login/">Login</a>
