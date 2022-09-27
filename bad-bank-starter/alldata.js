50dcc85afe99d3eaabba2d5d48c562a55f17015d
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-09-26 21:28:15 -0500

    Adds dynamic cards to alldata

--- bad-bank-starter/alldata.js
+++ bad-bank-starter/alldata.js
@@ -1,5 +1,27 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
+
+  function UserData(){
+    const cards = [];
+    for (let i = 0; i < ctx.users.length; i++) {
+
+        cards.push(<Card
+          key={i}
+          bgcolor="card text-white bg-danger mb-3"
+          header={"Name: " + ctx.users[i].name}
+          body={
+            "Email: " + ctx.users[i].email + 
+            " Password: " + ctx.users[i].password +
+            " Balance: " + ctx.users[i].balance}
+        />);
+    }
+    return cards;
+  }
+
+
+
+
+
   return (
     <div>
       <Card
@@ -8,18 +30,13 @@ function AllData(){
             status={"Password: " + ctx.current.password}
             body={"Username: "+ctx.current.username}
         />
-    <Card
-    
-            bgcolor="dark primary"
-            header="All Data in Storage"
-            status=""
-            body={<Card
-              bgcolor="card text-white bg-danger mb-3"
+        
+   <Card
+              bgcolor="card text-white bg-dark mb-3"
               header="All Data in Storage"
               status=""
-              body={JSON.stringify(ctx.users)}
-          />}
-        />
+              body={<UserData/>}
+            />
       </div>
   );
 }
