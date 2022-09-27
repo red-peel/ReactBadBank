c61239a9b84a3dbaae45d0e5fd07a841ce5ac4f2
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-09-26 21:54:47 -0500

    updates aesthetics

--- bad-bank-starter/footer.js
+++ bad-bank-starter/footer.js
@@ -2,7 +2,7 @@ function Footer(){
     return (
     <footer className="footer" >
         <div className="container">
-          <a className="text-muted" href="https://github.com/red-peel">made by E.G. using React</a>
+          <a className="text-muted" href="https://github.com/red-peel">made by E.G. using React <img src="gitcat.png" height="30px" ></img></a>
         </div>
       </footer>   
     );  
--- bad-bank-starter/home.js
+++ bad-bank-starter/home.js
@@ -2,7 +2,7 @@ function Home(){
   return (
     <Card
       bgcolor="dark"
-      header="Home"
+      header="the bad bank.™"
       title="Welcome to THE baddest bank"
       text="Please add a new account and/or select withdraw or deposit from the options above."
       body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
